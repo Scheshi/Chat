f19f8856c98239b6c19d68344b145f816ef5a2d7
Artem Darari <enjerudar@gmail.com>
2022-03-03 12:47:05 +0700

    Connect and disconnect client methods

--- Assets/Src/Features/Networking/Client.cs
+++ Assets/Src/Features/Networking/Client.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Client : MonoBehaviour
+namespace Src.Features.Networking
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Client
     {
-        
-    }
+        private int _reliableChannelIndex;
+        private int _unreliableChannelIndex;
+        private int _hostId;
+        private int _connectionId;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private bool _isConnected;
+        public bool IsConnected => _isConnected;
+
+        public void Connect(string address, int port)
+        {
+            NetworkTransport.Init();
+            ConnectionConfig config = new ConnectionConfig();
+            _reliableChannelIndex = config.AddChannel(QosType.Reliable);
+            _unreliableChannelIndex = config.AddChannel(QosType.Unreliable);
+            HostTopology topology = new HostTopology(config, 10);
+            _hostId = NetworkTransport.AddHost(topology, port);
+            _connectionId = NetworkTransport.Connect(_hostId, address, port, 0, out byte error);
+            NetworkError networkError = (NetworkError) error;
+            if (networkError == NetworkError.Ok)
+                _isConnected = true;
+            else
+                Debug.Log(networkError);
+        }
+
+        public void Disconnect()
+        {
+            if (!_isConnected)
+                return;
+            if (!NetworkTransport.Disconnect(_hostId, _connectionId, out byte error))
+                return;
+            NetworkError networkError = (NetworkError) error;
+            if (networkError == NetworkError.Ok)
+                _isConnected = false;
+            else
+                Debug.Log(networkError);
+        }
     }
 }
